f85521824c02c221b260fa41e1e407e586c4a927
Appolonchik <appolonov-27@yandex.ru>
2021-12-06 21:25:41 +0300

    new

--- Day17/D017/src/bisort/spros.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-#*****************************************
-#src\bisort\spros.py
-#*****************************************
-
-def sprosi_chislo():
-    return 42
---
+++ Day17/My Paket/src/bisort/spros.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# *****************************************
+# src\bisort\spros.py
+# *****************************************
+
+def sprosi_chislo():
+    return 42
+
+
