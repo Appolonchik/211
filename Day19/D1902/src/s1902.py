4dfcc334bb5499eacd9ec7f76d59fc90749cc08f
Appolonchik <appolonov-27@yandex.ru>
2021-12-11 21:18:40 +0300

    new

--- Day19/D1902/src/s1902.py
+++ Day19/D1902/src/s1902.py
@@ -8,11 +8,19 @@ print(data_path)
 data_path = abspath(data_path)
 print(data_path)
 
-with open(data_path, mode='rt', encoding= 'utf-8') as src:
-    for line in src:
-        line = line.strip()
-        print(line)
+write_path = join('..', 'data', 'new_elochka.txt')
+print(write_path)
+write_path = abspath(write_path)
+print(write_path)
 
+with open(write_path, 'wt', encoding='utf-8') as dst:
+    with open(data_path, encoding= 'utf-8', mode='rt') as src:
+        n = 0
+        for line in src:
+            n += 1
+            line = line.strip()
+            print(f'{n:3} {len(line):3} {len(line.split()):2} {line}', file=dst)
+            
 
 
 print('END')
