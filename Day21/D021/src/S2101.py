5deefc125787674353cf6db9c59eca9100720a74
Appolonchik <appolonov-27@yandex.ru>
2021-12-15 21:37:21 +0300

    new

--- Day21/D021/src/S2101.py
+++ Day21/D021/src/S2101.py
@@ -1,5 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# Импортируем библиотеки для работы с путями 
+from os.path import join, abspath
+import sqlite3
+
+base_path = join('..', 'Data', 'Learn.sqlite3')
+base_path = abspath(base_path)
+print(base_path)
+
+course_path = abspath(join('../Data/course.txt'))
+print(course_path)
+
+
+with sqlite3.connect(base_path) as con:
+    with open(course_path, mode='rt', encoding='cp1251') as src:
+        try:
+            cur = con.cursor()
+            for line in src:
+                line = line.strip()
+                if line:
+                    cnam, ccode, cdur = line.split(';')
+                    sdata = cnam.strip(), ccode.strip(), int(cdur)
+                    sql = '''INSERT INTO Course
+                    (CourseName, CourseCode, CourseDuration) 
+                    VALUES (?, ?, ?);'''
+                    cur.execute(sql, sdata)
+        except sqlite3.DatabaseError as err:
+            print(f'Ошибка 1: {err}')
+        else:
+            con.commit()
+            print(f'Все запросы добавлены в БД {base_path} выполнены')
+                    
 
 
 
